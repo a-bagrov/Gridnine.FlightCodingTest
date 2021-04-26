b626d5f5e022fbcbc75a6e24d55c5cde3163f1e4
Andrey Bagrov <andrey.bagrov@yandex.ru>
2021-04-26 16:59:56 +0300

    Add project files.

---
+++ Gridnine.FlightCodingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gridnine.FlightCodingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
